980dfbfa422095af167692b2721dd84f5eafb744
Mah <marinalarissacarpesrohrig@gmail.com>
2022-11-08 21:32:35 -0300

    desafio-triangulo com looping para criar um triangulo de *

--- 08112022/desafio-triangulo.js
+++ 08112022/desafio-triangulo.js
@@ -1,18 +1,17 @@
 // Escreva um programa que, dado um valor n, seja n>1 ,
-// imprima na tela um quadrado feito de asteriscos com lado de tamanho n 
+// imprima na tela um tiangrulo feito de asteriscos com lado de tamanho n 
 
-let n=2;
+let n=4;
 let string='';
 
 if (n<1){
     console.log("Número inválido!");
 }else {
     for (let row = 0; row<n; row++){
-        for (let column = 0; column<n; column++){
+        for (let column = 0; column<=row; column++){
             string+="*";
         }
         console.log(string)
         string='';
     }
 }
-

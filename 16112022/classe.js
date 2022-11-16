106e27e03616156722bf9ae3ec3bb6aa142c4b18
Mah <marinalarissacarpesrohrig@gmail.com>
2022-11-16 19:20:21 -0300

    exemplo de .map e de uso de classe

---
+++ 16112022/classe.js
@@ -0,0 +1,11 @@
+class myClass{
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+   }
+   
+   const user = new myClass("Marina", 25);
+   console.log(user.name)
+   console.log(user.age)
+   

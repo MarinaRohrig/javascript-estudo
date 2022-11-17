81c2a30651f6bca84914f401fbc4ec0c17cd085b
Mah <marinalarissacarpesrohrig@gmail.com>
2022-11-17 19:44:20 -0300

    exercicio modulo, import e export

---
+++ 17112022/index.js
@@ -0,0 +1,7 @@
+//import { currencyFormat,convert_to_cpf,cpfMask,addLeadingZeros,sortArray} from "./util/index.js";
+//import (currencyFormat)
+import * as Module from "./util/index-teste-export.js";
+
+
+console.log(Module.cpfMask('03707984060'));
+console.log(Module);
